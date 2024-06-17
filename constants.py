10002d07093d51f6a8afe35d3684c3f7f40442f1
Xian37 <lix24773@gmail.com>
2024-06-17 10:23:48 +0800

    add constant.py

--- constants.py
+++ constants.py
@@ -13,11 +13,16 @@ USEREVENT = 0 # to include this variable in order to the use of invincible item 
 MARGIN_WIDTH = 100
 SCREEN_WIDTH, SCREEN_HEIGHT = 1060+10, 790+10
 GAME_WIDTH = SCREEN_WIDTH - MARGIN_WIDTH
-
-player_path = "Game_Picture/cucu.png"
+ADD_ITEM_EVENT = pygame.USEREVENT + 5
+start_image_path = "Game_Picture/start.png"
+game_over_path = "Game_Picture/gameover.png"
+game_win_path = "Game_Picture/gamewin.png"
+#player_path = "Game_Picture/cucu.png"
 bomb_path = "Game_Picture/bomb.png"
-enemy_path = "Game_Picture/mi.png"
 flame_path = "Game_Picture/flame.png"
+rainbow_path = "Game_Picture/rainbow.jpeg"
+
+# Items
 health_path = "Game_Picture/health.png"
 speed_path = "Game_Picture/speed.png"
 invincible_path = "Game_Picture/star.png"
@@ -25,18 +30,21 @@ landmine_path = "Game_Picture/landmine.png"
 
 # Map Desert
 desert_road_path = "Game_Picture/sand.jpg"
-desert_wall_path = "Game_Picture/captus.png"
+desert_wall_path = "Game_Picture/cactus.png"
 desert_ob_path = "Game_Picture/rock.png"
 
 # Map Ocean
-ocean_road_path = "Game_Picture/sand.jpg"
-ocean_wall_path = "Game_Picture/forest.png"
-ocean_ob_path = "Game_Picture/forest2.jpg"
+ocean_road_path_1 = "Game_Picture/sea_background_1.jpg"
+ocean_road_path_2 = "Game_Picture/sea_background_2.jpg"
+ocean_road_path_3 = "Game_Picture/sea_background_3.jpg"
+ocean_road_path_4 = "Game_Picture/sea_background_4.jpg"
+ocean_wall_path_1 = "Game_Picture/coral2.png"
+ocean_ob_path = "Game_Picture/rock.png"
 
 # Map Forest
-forest_road_path = "Game_Picture/grass.png"
+forest_road_path = "Game_Picture/grass.jpg"
 forest_wall_path = "Game_Picture/forest.png"
-forest_ob_path = "Game_Picture/forest2.jpg"
+forest_ob_path = "Game_Picture/forest2.png"
 
 # Define colors
 WHITE = (255, 255, 255)
@@ -47,11 +55,25 @@ GREEN = (0, 255, 0)
 ORANGE = (255, 165, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255,255,0)
+LIGHTBLUE = (117,210,210)
+DARKBLUE = (2,2,85)
 
 # Map Path
-map1_img = "Game_Picture/forest2.jpg"
+map1_img = "Game_Picture/forest2.png"
 map2_img = "Game_Picture/sea.png"
 map3_img = "Game_Picture/desert.png"
+
+# Role Path
+
+role1_fox = "Game_Picture/fox2.png"
+role2_monkey = "Game_Picture/monkey.png"
+role3_cucu = "Game_Picture/cucu.png"
+
+# Enemy Path
+
+fox_enemy = "Game_Picture/panda.png"
+monkey_enemy = "Game_Picture/blond_guy.png"
+cucu_enemy = "Game_Picture/mi.png"
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Bomber Man')
@@ -59,8 +81,25 @@ programIcon = pygame.image.load('Game_Picture/bomb.png')
 pygame.display.set_icon(programIcon)
 
 map1_image = pygame.image.load(map1_img)
-map1_image = pygame.transform.scale(map1_image, (250, 250))
+map1_image = pygame.transform.scale(pygame.image.load(map1_img), (250, 250))
 map2_image = pygame.image.load(map2_img)
-map2_image = pygame.transform.scale(map2_image, (250, 250))
+map2_image = pygame.transform.scale(pygame.image.load(map2_img), (250, 250))
 map3_image = pygame.image.load(map3_img)
-map3_image = pygame.transform.scale(map3_image, (250, 250))
+map3_image = pygame.transform.scale(pygame.image.load(map3_img), (250, 250))
+
+role1_image = pygame.image.load(role1_fox)
+role1_image = pygame.transform.scale(pygame.image.load(role1_fox), (250, 250))
+role2_image = pygame.image.load(role2_monkey)
+role2_image = pygame.transform.scale(pygame.image.load(role2_monkey), (250, 250))
+role3_image = pygame.image.load(role3_cucu)
+role3_image = pygame.transform.scale(pygame.image.load(role3_cucu), (250, 250))
+
+start_image = pygame.image.load(start_image_path)
+gameover_image = pygame.image.load(game_over_path)
+gamewin_image = pygame.image.load(game_win_path)
+rainbow_image = pygame.image.load(rainbow_path)
+block_x = 10
+block_y = 265
+bar_width = 80
+bar_height = 525
+outline_rect = pygame.Rect(block_x, block_y, bar_width, bar_height)
