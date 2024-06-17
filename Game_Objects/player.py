41c85e57d8d8e71c8ad5e3cbdac5aa76fedc2292
Xian37 <lix24773@gmail.com>
2024-06-17 10:25:26 +0800

    add player

--- Game_Objects/player.py
+++ Game_Objects/player.py
@@ -3,8 +3,8 @@ from Game_Objects.game_object import GameObject
 from constants import *
 
 class Player(GameObject):
-    def __init__(self, x, y, size, color, speed, player_path, name='You'):
-        super().__init__(x, y, size, color, name=name)
+    def __init__(self, x, y, size, color, speed, health = 100, player_path='', name='You'):
+        super().__init__(x, y, size, color, health = 100, name=name)
         self.speed = speed
         # Initialize with maximum bombs #if didnt -1 it will have 3 bombs (X)
         self.bombs = MAX_BOMBS-1
@@ -14,6 +14,7 @@ class Player(GameObject):
         self.image = pygame.transform.scale(self.image, (self.size, self.size))
         self.bomb_image = pygame.image.load(bomb_path)
         self.bomb_image = pygame.transform.scale(self.bomb_image, (60, 60))
+        self.healthstate_color = RED
 
     def move(self, dx, dy, map_data):
         new_x = self.x + dx
@@ -23,7 +24,7 @@ class Player(GameObject):
             self.y = new_y
 
     def can_move(self, x, y, map_data):
-        if x < MARGIN_WIDTH or y < 0 or x + self.size > SCREEN_WIDTH or y + self.size > SCREEN_HEIGHT:
+        if x < MARGIN_WIDTH or y < 10 or x + self.size > SCREEN_WIDTH or y + self.size > SCREEN_HEIGHT:
             return False
         try:
             if (map_data[int((y - 10) // TILE_SIZE)][int((x - MARGIN_WIDTH) // TILE_SIZE)] != 0 or
@@ -52,7 +53,7 @@ class Player(GameObject):
         slot_y_start = 10
         for i in range(1,MAX_BOMBS+1):
             slot_y = slot_y_start + (i-1) * (slot_size + 10)
-            pygame.draw.rect(screen, BLACK, (slot_x, slot_y, slot_size, slot_size), 2)
+            pygame.draw.rect(screen, BLACK, (slot_x - 3, slot_y - 3, slot_size + 3, slot_size + 3), 3)
             if i < self.bombs:
                 screen.blit(self.bomb_image, (slot_x+10, slot_y+10))
                 # pygame.draw.circle(
@@ -64,6 +65,7 @@ class Player(GameObject):
                 screen.blit(self.bomb_image, (slot_x+10, slot_y+10))
                 # screen.blit(timer_text, (slot_x + slot_size // 2 - timer_text.get_width() //
                 #           2, slot_y + slot_size // 2 - timer_text.get_height() // 2))
+
 
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
