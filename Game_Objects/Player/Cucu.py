f6396e8bbe9ba2bdc4238ce9719c453f8b6363e4
Xian37 <lix24773@gmail.com>
2024-06-17 10:18:15 +0800

    add player folder

---
+++ Game_Objects/Player/Cucu.py
@@ -0,0 +1,26 @@
+import pygame
+from Game_Objects.game_object import GameObject
+from Game_Objects.player import Player
+from constants import *
+
+class Cucu(Player):
+    def __init__(self, x, y, size, color, speed, role3_cucu):
+        super().__init__(x, y, size, color, speed, health =100, player_path = role3_cucu, name = 'You')
+        self.speed = speed + 1.25
+    def draw_health_block(self, screen):
+        rainbow_image = pygame.image.load(rainbow_path)
+        font = pygame.font.Font(None, 45)
+        block_x = 10
+        block_y = 265
+        bar_width = 80
+        bar_height = 525
+        outline_rect = pygame.Rect(block_x - 3, block_y - 3, bar_width + 3, bar_height + 3)
+        fill_rect = pygame.Rect(block_x, block_y + bar_height-(self.health / 100) * bar_height, bar_width, (self.health / 100) * bar_height)
+        rainbow_image = pygame.transform.scale(rainbow_image, (80,525*(self.health / 100)))
+        if self.invincible == True:
+            #pygame.draw.rect(screen, BLACK, fill_rect)
+            screen.blit(rainbow_image, fill_rect)
+        else:
+            pygame.draw.rect(screen, RED, fill_rect)
+        pygame.draw.rect(screen, BLACK, outline_rect, 3)
+        screen.blit(font.render(str(self.health)+"%", True, BLACK), (10, 210))
---
+++ Game_Objects/Player/Monkey.py
@@ -0,0 +1,26 @@
+import pygame
+from Game_Objects.game_object import GameObject
+from Game_Objects.player import Player
+from constants import *
+
+class Monkey(Player):
+    def __init__(self, x, y, size, color, speed, role2_monkey):
+        super().__init__(x, y, size, color, speed, health = 125,player_path = role2_monkey, name = 'You')
+        self.health = 125
+    def draw_health_block(self, screen):
+        rainbow_image = pygame.image.load(rainbow_path)
+        font = pygame.font.Font(None, 45)
+        block_x = 10
+        block_y = 265
+        bar_width = 80
+        bar_height = 525
+        outline_rect = pygame.Rect(block_x - 3, block_y - 3, bar_width + 3, bar_height + 3)
+        fill_rect = pygame.Rect(block_x, block_y + bar_height-(self.health / 125) * bar_height, bar_width, (self.health / 125) * bar_height)
+        rainbow_image = pygame.transform.scale(rainbow_image, (80,525*(self.health / 125)))
+        if self.invincible == True:
+            #pygame.draw.rect(screen, BLACK, fill_rect)
+            screen.blit(rainbow_image, fill_rect)
+        else:
+            pygame.draw.rect(screen, RED, fill_rect)
+        pygame.draw.rect(screen, BLACK, outline_rect, 3)
+        screen.blit(font.render(str(self.health)+"%", True, BLACK), (10, 210))
