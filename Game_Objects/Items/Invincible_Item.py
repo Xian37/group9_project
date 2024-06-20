685dcda05fa101652cca9909d5de39be49e2f838
Xian37 <lix24773@gmail.com>
2024-06-21 03:02:22 +0800

    code_format

--- Game_Objects/Items/Invincible_Item.py
+++ Game_Objects/Items/Invincible_Item.py
@@ -4,11 +4,13 @@ from constants import *
 from Game_Objects.enemy import Enemy
 from Game_Objects.player import Player
 
+
 class InvincibleItem(GameObject):
     def __init__(self, x, y, size, color):
         super().__init__(x, y, size, color)
         self.image_invincible = pygame.image.load(invincible_path)
-        self.image_invincible = pygame.transform.scale(self.image_invincible, (TILE_SIZE, TILE_SIZE))
+        self.image_invincible = pygame.transform.scale(
+            self.image_invincible, (TILE_SIZE, TILE_SIZE))
 
     def apply(self, target):
         if isinstance(target, Player):
